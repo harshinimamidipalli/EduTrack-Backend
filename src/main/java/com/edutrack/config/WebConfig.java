b11000609e55e2ee6be6f29c5e049f6bb0809086
Pragmagic <pragmagic@Pragmagics-MacBook-Air.local>
2025-11-26 13:53:20 +0530

    Added actual backend source code

--- src/main/java/com/edutrack/config/WebConfig.java
+++ src/main/java/com/edutrack/config/WebConfig.java
@@ -1,17 +1,18 @@
 package com.edutrack.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/Users/pragmagic/Desktop/PROJECTS/edutrack/uploads/");
+                .addResourceLocations("file:" + System.getProperty("user.home") + "/EduTrackUploads/");
     }
 }
